42327c88fbd88a418425be9ddfd74df9b2f44130
Rizki Taufik Anindhita <rizki.anindhita@binus.ac.id>
2023-11-09 19:31:55 +0700

    membuat route

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AskQuestion = () => {
+  return <div>AskQuestion Page</div>;
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">nextjs learning go</h1>
-    </div>
-  );
-}
